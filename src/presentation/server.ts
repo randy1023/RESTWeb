768ba277bd79377a199972327134416ac6997ee9
Randy Gonzalez <gonzalezmanaurer@gmail.com>
2025-05-24 20:01:27 -0400

    se agrgeo una web spa y con mi web server sirvos la url

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+
 export class Server {
   private app = express();
   constructor() {}
@@ -8,6 +10,12 @@ export class Server {
 
     //* Public Folder
     this.app.use(express.static("public"));
+
+    this.app.get("/{*splat}", (req, res) => {
+      const indexPath = path.join(`${__dirname}../../../public/index.html`);
+      res.sendFile(indexPath);
+      return;
+    });
 
     this.app.listen(3000, () => {
       console.log("Server running on port 3000");
